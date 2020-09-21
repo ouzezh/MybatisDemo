7b9f82c8007ca4a8a1b3e56a6f6a7ae72b13197e
ouzezh <ouzezh@163.com>
2020-09-21 17:38:07 +0800

    mybatis

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
-## 多数据源支持
+## Spring动态据源源
 ```
+  扫描所有MapperLocation，使用Aspect切换AbstractRoutingDataSource来源
+
   application.yml
 
   com.ozz.mybatis.component.db.routing.*
@@ -7,6 +9,14 @@
   MybatisApp
     @Import({RoutingDataSourceConfig.class})
     @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+```
+
+## 访问多个数据库
+```
+扫描不同basePackages及MapperLocation的路径，使用对应接口自动切换数据源
+
+@Import({RoutingDataSourceConfig.class, SecondDataSourceConfig.class})
+配置多个数据源，分别使用不同的mapper文件和model类
 ```
 
 ## log4jdbc
--- src/main/java/com/ozz/mybatis/model/TestObject.java
+++ src/main/java/com/ozz/mybatis/model/TestObject.java
@@ -1,12 +1,14 @@
 package com.ozz.mybatis.model;
 
-import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 @TableName("city")
 public class TestObject extends BaseObject {
-    //  @TableField(exist = false)
+//  @TableField(exist = false)// 属性在数据库中不存在
+//  @TableField("id")// 属性对应字段名
 
-    @TableField("id")
-    private Integer id;
+  @TableId(type = IdType.AUTO)// 数据库ID自增
+  private Integer id;
 }
