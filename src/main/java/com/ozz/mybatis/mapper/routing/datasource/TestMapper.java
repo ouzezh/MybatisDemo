82eb88eb2fdd0a8582fa7262ee303c14c1e76671
ouzezh <ouzezh@163.com>
2020-09-22 10:07:13 +0800

    mybatis

--- src/main/java/com/ozz/mybatis/mapper/routing/datasource/TestMapper.java
+++ src/main/java/com/ozz/mybatis/mapper/routing/datasource/TestMapper.java
@@ -1,15 +1,13 @@
 package com.ozz.mybatis.mapper.routing.datasource;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ozz.mybatis.model.TestObject;
 import java.util.Date;
 import java.util.List;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ozz.mybatis.model.BaseObject;
-import com.ozz.mybatis.model.TestObject;
 import org.apache.ibatis.annotations.Param;
 
 public interface TestMapper extends BaseMapper<TestObject> {
   public List<String> selectTest();
   public int updateTest(@Param("ids") List<Long> ids);
-  public Date selectTxTimestamp(long id);
+  public Date selectTxTimestamp(@Param("id") long id);
 }
